467a3306c31f32c9a0276ba945231f420a3bcded
Serj3000 <52423079+Serj3000@users.noreply.github.com>
2019-09-14 20:33:41 +0300

    3 commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $categories=[
+            ['name'=>'Features', 'slug'=>'features'],
+            ['name'=>'Food', 'slug'=>'food'],
+            ['name'=>'Travel', 'slug'=>'travel'],
+            ['name'=>'Recipe', 'slug'=>'recipe'],
+            ['name'=>'Bread', 'slug'=>'bread'],
+            ['name'=>'Breakfast', 'slug'=>'breakfast'],
+            ['name'=>'Meat', 'slug'=>'meat'],
+            ['name'=>'Fastfood', 'slug'=>'fastfood'],
+            ['name'=>'salad', 'slug'=>'features'],
+            ['name'=>'Soup', 'slug'=>'soup']
+        ];
+
+        DB::table('categories')->insert($categories);
+
     }
 }
